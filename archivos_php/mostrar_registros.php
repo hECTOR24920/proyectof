c9a25d302ef5b9fb3496a63bdc70cf0bb24a3f17
ANTONIOF12 <87738217+ANTONIOF12@users.noreply.github.com>
2021-07-26 13:57:42 -0500

    Update mostrar_registros.php

--- archivos_php/mostrar_registros.php
+++ archivos_php/mostrar_registros.php
@@ -1,35 +1,40 @@
-<? php
-require_once ( "conexion.php" );
+<?php
+require_once("conexion.php");
 
-$ solicitud = "SELECCIONAR * DE datos_usuarios PEDIR POR Nombre ASC" ;
-$ resultado = mysqli_query ( $ conexión , $ solicitud );
+$solicitud = "SELECT * FROM datos_usuarios ORDER BY Nombre ASC";
+$resultado = mysqli_query($conexion, $solicitud);
 
-$ sub_titulo = "Usuarios Registrados" ;
-echo  "\ t <h2>" . $ sub_titulo . "</h2> \ n" ;
+$sub_titulo = "Usuarios Registrados";
+echo "\t<h2>" . $sub_titulo . "</h2>\n";
 
-echo  "\ t <table cellpadding = '10 'cellspacing = '10'>
-\ t \ t <tr>
-\ t \ t \ t <th> Nombres </th>
-\ t \ t \ t <th> Apellidos </th>
-\ t \ t \ t <th> Correos </th>
-\ t \ t \ t <th> Teléfonos </th>
-\ t \ t \ t <th> Acción </th>
-\ t \ t </tr> \ n " ;
-while ( $ usuarios_registrados = mysqli_fetch_array ( $ resultado )) {
-	$ usuario_nombre = $ usuarios_registrados [ 'Nombre' ];
-	$ usuario_a Apellidos = $ usuarios_registrados [ 'Apellido' ];
-	$ usuario_correo = $ usuarios_registrados [ 'Correo' ];
-	$ usuario_telefono = $ usuarios_registrados [ 'Telefono' ];
-	$ usuario_id = $ usuarios_registrados [ 'ID' ];
-	$ enlace_borrar = "<a id = 'enlace_borrar' href = '.. / Registro_Simple / archivos_php / borrar_registros.php? id =" . $ usuario_id . "'> Eliminar </a>" ;
+echo "\t<table cellpadding='10' cellspacing='10'>
+\t\t<tr>
+\t\t\t<th>Nombres</th>
+\t\t\t<th>Domicilio</th>
+\t\t\t<th>Telefono</th>
+\t\t\t<th>Nick</th>
+\t\t\t<th>Password</th>
+\t\t\t<th>Tipo de usuario</th>
+\t\t\t<th>Acción</th>
+\t\t</tr>\n";
+while($usuarios_registrados = mysqli_fetch_array($resultado)){
+	$usuario_nombre = $usuarios_registrados['Nombre'];
+	$usuario_Domicilio = $usuarios_registrados['Domicilio'];
+	$usuario_telefono = $usuarios_registrados['Telefono'];
+	$usuario_nick = $usuarios_registrados['nick'];
+	$usuario_password = $usuarios_registrados['Password'];
+	$usuario_tipo_de_usuario = $usuarios_registrados['tipo de usuario '];
+	$usuario_id = $usuarios_registrados['ID'];
+	$enlace_borrar = "<a id='enlace_borrar' href='../Registro_Simple/archivos_php/borrar_registros.php?id=".$usuario_id."'>Eliminar</a>";
 	
-	echo  "\ t \ t <tr> \ n" ;
-	echo  "\ t \ t \ t <td>" . $ usuario_nombre . "</td> \ n" ;
-	echo  "\ t \ t \ t <td>" . $ usuario_a Apellidos . "</td> \ n" ;
-	echo  "\ t \ t \ t <td>" . $ usuario_correo . "</td> \ n" ;
-	echo  "\ t \ t \ t <td>" . $ usuario_telefono . "</td> \ n" ;
-	echo  "\ t \ t \ t <td>" . $ enlace_borrar . "</td> \ n" ;
-	echo  "\ t \ t </tr> \ n" ;
+	echo "\t\t<tr>\n";
+	echo "\t\t\t<td>" . $usuario_nombre . "</td>\n";
+	echo "\t\t\t<td>" . $usuario_Domicilio . "</td>\n";
+	echo "\t\t\t<td>" . $usuario_nick. "</td>\n";
+	echo "\t\t\t<td>" . $usuario_password . "</td>\n";
+	echo "\t\t\t<td>" . $usuario_tipo_de_usuario . "</td>\n";
+	echo "\t\t\t<td>" . $enlace_borrar . "</td>\n";
+	echo "\t\t</tr>\n";
 }
-echo  "\ t </table> \ n" ;
+echo "\t</table>\n";
 ?>
