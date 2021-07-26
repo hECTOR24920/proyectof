6c3b60e6a9d20a989817c9c7664558b2244a60d2
ANTONIOF12 <87738217+ANTONIOF12@users.noreply.github.com>
2021-07-26 13:55:34 -0500

    Update borrar_registros.php

--- archivos_php/borrar_registros.php
+++ archivos_php/borrar_registros.php
@@ -1,11 +1,10 @@
-  
-<? php 
-require_once ( "conexion.php" );
+  <?php 
+require_once("conexion.php");
 
-$ id = $ _GET [ 'id' ];
+$id = $_GET['id'];
 
-$ seg_id = mysqli_real_escape_string ( $ conexión , $ id );
-$ borrar = "ELIMINAR DE datos_usuarios DONDE ID = {$ seg_id}" ;
-$ resultado = mysqli_query ( $ conexión , $ borrar );
-encabezado ( "ubicación: ../index.php" );
+$seg_id = mysqli_real_escape_string($conexion, $id);
+$borrar = "DELETE FROM datos_usuarios WHERE ID={$seg_id}";
+$resultado = mysqli_query($conexion, $borrar);
+header("location: ../index.php");
 ?>
